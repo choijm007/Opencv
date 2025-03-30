25d67780887c318fb6ab9a369f431eafd9410f23
choijm007 <125243677+choijm007@users.noreply.github.com>
2025-03-30 13:32:35 +0900

    Add files via upload

---
+++ 2020203037_최재민_HW2/HW2-1/HW2-1.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
+
+
+
+int main()
+{
+    Mat black(256, 256, CV_8UC1);
+    Mat white(256, 256, CV_8UC1);
+    Mat rec(256, 256, CV_8UC1);
+    Mat circle(256, 256, CV_8UC1);
+
+    int height = rec.rows;
+    int width = rec.cols;
+
+    memset(black.data, 0, 256 * 256);
+    memset(white.data, 255, 256 * 256);
+
+
+    memset(rec.data, 0, 256 * 256);
+    memset(circle.data, 0, 256 * 256);
+
+    int recsize = 100;
+
+    int num = int((height - recsize) / 2);
+
+    for (int j = num; j < height - num; j++) for (int i = num; i < width - num; i++) {
+        rec.data[width * j + i] = 255;
+    }
+
+    for (int j = 0; j < height; j++) for (int i = 0; i < width; i++) {
+        if (sqrt(pow(abs(i - 128), 2) + pow(abs(j - 128), 2)) < recsize)
+            circle.data[width * j + i] = 255;
+    }
+
+
+    imshow("black", black);
+    imshow("white", white);
+    imshow("rec", rec);
+    imshow("circle", circle);
+
+
+    waitKey();
+
+    return 0;
+}
---
+++ 2020203037_최재민_HW2/HW2-3/resource.h
@@ -0,0 +1,27 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCopencv.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCOPENCV_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define ID_FILE_FILEOPEN                32771
+#define ID_FILE_FILESAVE                32772
+#define ID_FILE_FILECLOSE               32773
+#define ID_EDIT_CONVERTCOLOR            32774
+#define ID_CONVERTCOLOR_CONVERTGARY     32775
+#define ID_CONVERTCOLOR_CONVERT         32776
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
